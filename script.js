479a0cfd460627c6704335bfd697371738a65771
Vishwakse <vishwaksen2275@gmail.com>
2026-02-04 22:39:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let targetElement = document.getElementById('level');
+
+let domLevel = 0;
+
+while (targetElement) {
+    domLevel++;
+    targetElement = targetElement.parentElement;
+}
+
+alert("The level of the element is: " + domLevel);
